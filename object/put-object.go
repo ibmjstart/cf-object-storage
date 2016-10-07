a590a31760a220000a357bcaa91b6a7ea8641761
Ryan Soley <resoley@us.ibm.com>
2016-10-07 14:37:28 -0400

    Add flag parsing logic for put-object

--- object/put-object.go
+++ object/put-object.go
@@ -1,6 +1,35 @@
 package object
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	fp "path/filepath"
+)
+
+type flagVal struct {
+	Rename_flag string
+}
+
+func parseArgs(args []string) (*flagVal, error) {
+	flagSet := flag.NewFlagSet("flagSet", flag.ContinueOnError)
+
+	// Define flag to default to original filename
+	rename := flagSet.String("n", fp.Base(args[1]), "Rename object before uploading")
+
+	// Parse optional flags if they have been provided
+	if len(args) > 2 {
+		err := flagSet.Parse(args[2:])
+		if err != nil {
+			return nil, fmt.Errorf("Failed to parse flags: %s", err)
+		}
+	}
+
+	flagVals := flagVal{
+		Rename_flag: string(*rename),
+	}
+
+	return &flagVals, nil
+}
 
 func main() {
 	fmt.Println("put-object")
